8faedaf1575900c8422d0567baa087e33272fde7
Julia Hansbrough <julia@edx.org>
2014-05-10 03:58:06 -0400

    Cleanup

--- README.md
+++ README.md
@@ -10,13 +10,8 @@ I got bored and decided I wanted to code up my own version one evening.
 So far, all it does is let you play against a very dumb computer opponent.  Future TODOs for next time I get bored:
 
 - display opponent's hand
-- display hands in a more sensible fashion
 - make the computer more clever than rand()
 - forbid impossible card selections / moves
 - add new rules
-- make the board an object; we use the "flat board" function errywhere
-- edit out the hardcoded 3x3 stuff, allow arbitrary board size
-- make cards.txt an arg, make it a default rather than hardcoded in
-- better turn handling
 - more cards
 - let you choose cards
--- triple_triad.py
+++ triple_triad.py
@@ -5,185 +5,241 @@ The Triple Triad card game, of Final Fantasy VIII, now playable in your terminal
 import random
 import sys
 import string
+import itertools
 from termcolor import colored
 
-BLACK, RED, GREEN, YELLOW, cyan, MAGENTA, CYAN, WHITE = range(8)
+# --------------- OBJECTS ---------------
 
-# --------------- MODELS ---------------
+class Game(object):
+    def __init__(self, deck, num_players=2):
+        """
+        deck: list of Cards that Players can use to make their hands
+        num_players: number of players in this game.  This is a small lie;
+        at the moment >2 players is not supported.
+        """
+        self.board = Board()
+        self.players = []
+        self.players.append(Player("human", load_hand("cyan", deck)))
+        self.players.append(Player("computer", load_hand("red", deck)))
+        self.player_loop = itertools.cycle(self.players)
+
+    def calculate_scores(self):
+        p1_score = 0
+        p2_score = 0
+        for card in (self.players[0].hand + self.players[1].hand + self.board.flat_list()):
+            if card is not None:
+                if card.color == "cyan":
+                    p1_score = p1_score + 1
+                if card.color == "red":
+                    p2_score = p2_score + 1
+        print "Player 1: " + str(p1_score)
+        print "Player 2: " + str(p2_score)
+
+    def run(self):
+        self.board.display()
+        running = True
+        while running:
+            current_player = next(self.player_loop)
+            if current_player.type == "human":
+                card = prompt_card_choice(current_player.hand)
+                location = prompt_location_choice(self.board)
+                self.board.place(card, location)
+                current_player.hand.remove(card)
+                print ""
+                self.board.display()
+                self.calculate_scores()
+                print ""
+
+            else:
+                # Computer opponents always place a random card in a random location as of now.
+                card = random.choice(current_player.hand)
+                location = random.choice([i for i, j in enumerate(self.board.flat_list()) if j is None])
+                self.board.place(card, location)
+                current_player.hand.remove(card)
+                print ""
+                self.board.display()
+                self.calculate_scores()
+                print ""
+
+            if None not in self.board.flat_list():
+                # All the board slots have been filled; end the game.
+                running = False
+
+
+class Player(object):
+    def __init__(self, type, hand):
+        self.type = type
+        self.hand = hand
+
 
 class Card(object):
-	def __init__(self, top, right, bottom, left, color):
-		self.top = top
-		self.right = right
-		self.bottom = bottom
-		self.left = left
-		self.color = color
+    def __init__(self, top, right, bottom, left, color):
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.left = left
+        self.color = color
 
-	def flip(self):
-		if self.color == "cyan":
-			self.color = "red"
-		else:
-			self.color = "cyan"
+    def flip(self):
+        # If support for >2 players is added, will need support >2 colors
+        if self.color == "cyan":
+            self.color = "red"
+        else:
+            self.color = "cyan"
 
-# --------------- Card interaction + game logic ---------------
+    def display(self):
+        print "  -------"
+        print "  |  " + str(self.top) + "  |"
+        print "  | " + str(self.left) + " " + str(self.right) + " |"
+        print "  |  " + str(self.bottom) + "  |"
+        print "  -------"
 
-def calculate_scores(p1_hand, p2_hand, board):
-	p1_score = 0
-	p2_score = 0
-	for card in (p1_hand + p2_hand + [x for sublist in board for x in sublist]):
-		if card is not None:
-			if card.color == "cyan":
-				p1_score = p1_score + 1
-			if card.color == "red":
-				p2_score = p2_score + 1
-	print "Player 1: " + str(p1_score)
-	print "Player 2: " + str(p2_score)
 
-def place(card, location, board, hand):
-	""" Place Card card in (row, column) location """
-	row = location / 3
-	column = location % 3
+class Board(object):
+    def __init__(self, width=3, height=3):
+        self.board = []
+        for i in range(0, height):
+            self.board.append([None for i in range(0, width)])
+        self.width = width
+        self.height = height
 
-	board[row][column] = card
-	hand.remove(card)
+    def place(self, card, location):
+        row = location / self.height
+        column = location % self.width
 
-	if not (row-1 < 0):
-		if board[row-1][column] is not None:
-			if board[row-1][column].bottom < board[row][column].top:
-				board[row-1][column].flip()
+        self.board[row][column] = card
 
-	if not (column+1 > 2):
-		if board[row][column+1] is not None:
-			if board[row][column+1].left < board[row][column].right:
-				board[row][column+1].flip()
+        if row-1 >= 0:
+            if self.board[row-1][column] is not None:
+                if self.board[row-1][column].bottom < self.board[row][column].top:
+                    if self.board[row-1][column].color != self.board[row][column].color:
+                        self.board[row-1][column].flip()
 
-	if not (row+1 > 2):
-		if board[row+1][column] is not None:
-			if board[row+1][column].top < board[row][column].bottom:
-				board[row+1][column].flip()
+        if column+1 < self.width:
+            if self.board[row][column+1] is not None:
+                if self.board[row][column+1].left < self.board[row][column].right:
+                    if self.board[row][column+1].color != self.board[row][column].color:
+                        self.board[row][column+1].flip()
 
-	if not (column-1 < 0):
-		if board[row][column-1] is not None:
-			if board[row][column-1].right < board[row][column].left:
-				board[row][column-1].flip()
+        if row+1 < self.height:
+            if self.board[row+1][column] is not None:
+                if self.board[row+1][column].top < self.board[row][column].bottom:
+                    if self.board[row+1][column].color != self.board[row][column].color:
+                        self.board[row+1][column].flip()
 
-def load_deck(file):
-	deck = []
-	for line in open(file):
-		if '#' not in line:
-			card_descriptor = [int(x) for x in line.split(',')]
-			card_descriptor.append(None)
-			deck.append(Card(*card_descriptor))
-	return deck
+        if column-1 >= 0:
+            if self.board[row][column-1] is not None:
+                if self.board[row][column-1].right < self.board[row][column].left:
+                    if self.board[row][column-1].color != self.board[row][column].color:
+                        self.board[row][column-1].flip()
 
-def load_hand(color, deck):
-	hand = []
-	for i in range(0,5):
-		hand.append(deck.pop(random.randrange(len(deck))))
-	for card in hand:
-		card.color = color
-	return hand
+    def flat_list(self):
+        """
+        Useful for giving us 'flat', 2D representations of the board,
+        e.g. when we want to select a square on the board at random.
+        """
+        return [x for sublist in self.board for x in sublist]
+
+    # UI functions for the board, inasmuch as this counts as UI...
+
+    def display(self):
+        for i in range(0, self.height):
+            self._print_boundary()
+            self._print_toprow(self.board[i])
+            self._print_midrow(self.board[i], i)
+            self._print_bottomrow(self.board[i])
+        self._print_boundary()
+
+    def _print_boundary(self):
+        print "-------------------------"
+
+    def _print_toprow(self, cards):
+        display = []
+        print "|",
+        for card in cards:
+            if card is not None:
+                print " ",
+                print colored(str(card.top), card.color),
+                print "  |",
+            else:
+                print "      |",
+        print ""
+
+    def _print_bottomrow(self, cards):
+        display = []
+        print "|",
+        for card in cards:
+            if card is not None:
+                print " ",
+                print colored(str(card.bottom), card.color),
+                print "  |",
+            else:
+                print "      |",
+        print ""
+
+    def _print_midrow(self, cards, letter_num):
+        letter_num = (letter_num * 3) + 97
+        display = []
+        print "|",
+        for card in cards:
+            if card is not None:
+                print colored(str(card.left), card.color),
+                print " ",
+                print colored(str(card.right), card.color),
+                print "|",
+            else:
+                print "  " + chr(letter_num) + "   |",
+            letter_num = letter_num + 1
+        print ""
+
+# --------------- Helper functions ---------------
+
+def load_deck(file='cards.txt'):
+    deck = []
+    for line in open(file):
+        if '#' not in line:
+            card_descriptor = [int(x) for x in line.split(',')]
+            card_descriptor.append(None)
+            deck.append(Card(*card_descriptor))
+    return deck
+
+def load_hand(color, deck, size=5):
+    hand = []
+    for i in range(0, size):
+        hand.append(deck.pop(random.randrange(len(deck))))
+    for card in hand:
+        card.color = color
+    return hand
 
 def prompt_location_choice(board):
-	selection = raw_input("Place card where? ")
-	return ord(selection.strip()) - 97
+    flattened = board.flat_list()
+    while True:
+        selection = raw_input("Place card where? ")
+        for spot, letter in zip(board.flat_list(), string.ascii_lowercase):
+            if selection == letter:
+                location = ord(selection.strip()) - 97
+                if flattened[location] is None:
+                    return location
+        print "Invalid choice, try again."
 
 def prompt_card_choice(hand):
-	print "Your hand: "
-	for card, letter in zip(hand, string.ascii_lowercase):
-		print letter + ". " + str([card.top, card.left, card.right, card.bottom])
-	selection = raw_input("Select your card: ")
-	for card, letter in zip(hand, string.ascii_lowercase):
-		if letter == selection:
-			return card
-
-# --------------- UI, inasmuch as this counts as UI ---------------
-
-def print_board(board):
-	for i in range(0,3):
-		_print_boundary()
-		_print_toprow(board[i][0], board[i][1], board[i][2])
-		_print_midrow(board[i][0], board[i][1], board[i][2], i)
-		_print_bottomrow(board[i][0], board[i][1], board[i][2])
-	_print_boundary()
-
-def _print_boundary():
-	print "-------------------------"
-
-def _print_toprow(left, middle, right):
-	display = []
-	print "|",
-	for card in [left, middle, right]:
-		if card is not None:
-			print " ",
-			print colored(str(card.top), card.color),
-			print "  |",
-		else:
-			print "      |",
-	print ""
-
-def _print_bottomrow(left, middle, right):
-	display = []
-	print "|",
-	for card in [left, middle, right]:
-		if card is not None:
-			print " ",
-			print colored(str(card.bottom), card.color),
-			print "  |",
-		else:
-			print "      |",
-	print ""
-
-def _print_midrow(left, middle, right, letter_num):
-	letter_num = (letter_num * 3) + 97
-	display = []
-	print "|",
-	for card in [left, middle, right]:
-		if card is not None:
-			print colored(str(card.left), card.color),
-			print " ",
-			print colored(str(card.right), card.color),
-			print "|",
-		else:
-			print "  " + chr(letter_num) + "   |",
-		letter_num = letter_num + 1
-	print ""
+    print "Your hand: "
+    for card, letter in zip(hand, string.ascii_lowercase):
+        print letter + "."
+        card.display()
+    while True:
+        selection = raw_input("Select your card: ")
+        for card, letter in zip(hand, string.ascii_lowercase):
+            if letter == selection:
+                return card
+        print "Invalid choice, try again."
 
 # --------------- Main game loop ---------------
 
 def main():
-	""" Core game loop """
-	print "WELCOME TO TRIPLE TRIAD"
-	print "Insert shuffle or boogie theme here"
-	deck = load_deck('cards.txt')
-	p1_hand = load_hand("cyan", deck)
-	p2_hand = load_hand("red", deck)
-	board = []
-	for i in range(0,3):
-		board.append([None, None, None])
-	game = True
-	print_board(board)
-	turn = "human"
-	while game:
-		if turn == "human":
-			card = prompt_card_choice(p1_hand)
-			location = prompt_location_choice(board)
-			place(card, location, board, p1_hand)
-			print ""
-			print_board(board)
-			winning = calculate_scores(p1_hand, p2_hand, board)
-			print ""
-			turn = "computer"
-
-		else:
-			card = random.choice(p2_hand)
-			location = random.choice([i for i, j in enumerate([x for sublist in board for x in sublist]) if j is None])
-			place(card, location, board, p2_hand)
-			print_board(board)
-			winning = calculate_scores(p1_hand, p2_hand, board)
-			turn = "human"
-
-		if None not in [item for sublist in board for item in sublist]:
-			game = False
-
+    """ Core game loop """
+    print "WELCOME TO TRIPLE TRIAD"
+    deck = load_deck()
+    Game(deck).run()
+    
 main()
